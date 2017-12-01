b5c4b39c791217cb89d3fde9998df4bf2124e95f
ttymarucr <ttymarucr@gmail.com>
2017-12-01 11:42:40 -0600

    Se agrega la funcionalidad de get al api de ML

--- app.js
+++ app.js
@@ -6,40 +6,6 @@ var bodyParser = require('body-parser');
 var get = require("./routes/getAPI.js");
 var post = require("./routes/postAPI.js");
 
-
-var azureQueue = require('azure-queue-node');
-azureQueue.setDefaultClient({
-  accountUrl: 'https://tbdix.queue.core.windows.net/tbdixfaultcodes',
-  accountName: 'tbdix',
-  accountKey: 'YI/QJHierS3d6Ckp6o4wo/B5BTM+odfQHYDLuw6BA7N5SXTN6aAKoo9I0/ZVjg76n/AxP8MtnXGSOdU3ZpgPPg=='
-});
-
-var client = azureQueue.getDefaultClient();
-client.createQueue('taskqueue', true, function(data){
-    console.log("created: " + data);
-});
-
-client.putMessage('taskqueue', {
-  value1: 'ABCDEFG'
-}, function(err, data) {
-    console.log(err);
-    console.log(data);
-  // err is null 
-  // data is undefined 
-});
-client.getMessages('taskqueue', {maxMessages: 10}, function(err, data) {
-    console.log(err);
-    console.log(data);
-  // err is null 
-  // data contains array with to 10 queue message objects 
-});
-client.deleteMessage('taskqueue', 'messageId', 'popReceipt', function(err, data) {
-    console.log(err);
-    console.log(data);
-  // err is null 
-  // data is undefined 
-});
-
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -47,7 +13,7 @@ app.use(bodyParser.json({type: 'application/json'}));
 app.use(cookieParser());
 
 app.use('/ml', get);
-app.use('/iot', post);
+//app.use('/iot', post);
 
 module.exports = app;
 
--- routes/getAPI.js
+++ routes/getAPI.js
@@ -8,11 +8,17 @@ web3 = new web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 var contracts = [];
 var TruffleContract = require('truffle-contract');
-var SensorABI = require('../build/contracts/Sensor.json');
+var ActorABI = require('../build/contracts/Actor.json');
+var VehiculeABI = require('../build/contracts/Vehicule.json');
+var VehiculeDatabaseABI = require('../build/contracts/VehiculeDatabase.json');
 
 //contracts init
-contracts.Sensor = TruffleContract(SensorABI);
-contracts.Sensor.setProvider(web3.currentProvider);
+contracts.Actor = TruffleContract(ActorABI);
+contracts.Actor.setProvider(web3.currentProvider);
+contracts.Vehicule = TruffleContract(VehiculeABI);
+contracts.Vehicule.setProvider(web3.currentProvider);
+contracts.VehiculeDatabase = TruffleContract(VehiculeDatabaseABI);
+contracts.VehiculeDatabase.setProvider(web3.currentProvider);
 
 //express app
 var app = express();
@@ -20,19 +26,50 @@ var router = express.Router();
 const path = require('path');
 
 /* THIS API READS FROM THE BLOCKCHAIN AND SERVES TO MACHINE LEARNING SERVICE */
-router.get('/', function (req, res) {
-  contracts.Sensor.deployed().then(instance=>{
-    return instance.sensorData(web3.eth.accounts[0]);
-  }).then(result=>{
+router.get('/:actor/:vin', function (req, res) {
+  var response = {};
+  var vehiculeInstance;
+  contracts.Actor.at(req.params.actor).then(actor => {
+    return actor.database();
+  }).then(databaseAddress => {
+    return contracts.VehiculeDatabase.at(databaseAddress);
+  }).then(database => {
+    return database.vins(web3.fromUtf8(req.params.vin));
+  }).then(vehiculeAddress => {
+    return contracts.Vehicule.at(vehiculeAddress);
+  }).then(vehicule => {
+    vehicule.OnActionEvent({}, { fromBlock: 0, toBlock: 'latest' }).get((error, result) => {
+      response["history"] = [];
+      result.forEach(row => {
+        response["history"].push({
+          event : row.args._event.c[0],
+          rerefence : row.args._ref,
+          description : row.args._description,
+          timestamp : new Date(row.args._timestamp.c[0] * 1000),
+          blockNumber : row.args._blockNumber.c[0]
+        });
+      });
+    });
+    return vehicule.getState();
+  }).then(state => {
+    response["state"] = {
+      brand : state[0],
+      model : state[1],
+      type : state[2],
+      engine : state[3],
+      extras : state[4],
+      year : state[6],
+      vin : web3.toUtf8(state[5]),
+      lastUpdate : state[7],
+    };
     res.json(
       {
-          "blockNumber": web3.eth.blockNumber,
-          "data" : result
+        "response": response
       }
     );
-  }).catch(error => { 
-    console.log('error', error);
-    res.json({"error":error.message});
+  }).catch(error => {
+    //console.log('error', error);
+    res.json({ "error": error.message });
   });
 });
 
